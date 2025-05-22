8c6f411fce88a0834d59f5d3432fd84fda1f5a8c
daniel.marica <livius.marica@student.vinci.be>
2025-05-22 20:02:30 +0100

    T06.3 Mise à jour du theme

--- lib/chapitre6_SHAREDPREFERENCE_DBSQLITE/tuto6/views/settings.dart
+++ lib/chapitre6_SHAREDPREFERENCE_DBSQLITE/tuto6/views/settings.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../view_model/theme_view_model.dart';
+
 import '../widgets/nav_bar.dart';
+import '../widgets/color_picker.dart';
 
 class Settings extends StatelessWidget {
   const Settings({super.key});
@@ -8,8 +12,25 @@ class Settings extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: navBar(context, 'Settings'),
-      body: const Center(
-        child: Text('Settings'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Consumer<ThemeViewModel>(
+          builder: (context, themeViewModel, child) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text('Pick a main color:'),
+                const SizedBox(height: 16.0),
+                ColorPicker(
+                  selectedColor: themeViewModel.mainColor,
+                  onColorSelected: (color) {
+                    themeViewModel.mainColor = color;
+                  },
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
